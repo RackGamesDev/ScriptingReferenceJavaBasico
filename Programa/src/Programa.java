3b19e8a61c117b76c6881c6a283813f54db499b0
yester <yesterdiscord7@gmail.com>
2023-12-01 20:07:03 +0100

    a

--- Programa/src/Programa.java
+++ Programa/src/Programa.java
@@ -4,6 +4,7 @@
 import java.util.Scanner;//importar un paquete (vienen de java.x javax.x org.x jdk.x o de personalizados) (en este caso importa lo necesario para introducir datos por consola)
 import unpaquete.*;//importando paquete ya creado (en este paquete se explican las clases)
 import javax.swing.*;//paquete para interfaces graficas (casi obsoleto) (* importa todo el paquete)
+import java.util.*;
 
 
 
@@ -103,6 +104,13 @@ public class Programa {//clase llamada igual que el archivo, esta es la que inic
         funcion1();//ejecutando una funcion (como es estatica se puede ejecutar sin instanciar una clase)
         funcion2(1,5);//con parametros
         numero1 = funcion3(3, 6);//funcion que devuelve un dato
+
+
+        //java.util:
+        GregorianCalendar calendario = new GregorianCalendar(2023, 0, 23);//guarda un calendario (enero es 0)
+        System.out.println(calendario.YEAR);//tiene sus funciones y propiedades
+        Date fecha = calendario.getTime();//guarda una fecha, este objeto tambien tiene sus funciones y demas
+
     }
 
     static void funcion1(){//una funcion que ejecuta lineas de codigo
--- Programa/src/unpaquete/clases.java
+++ Programa/src/unpaquete/clases.java
@@ -2,7 +2,7 @@
 package unpaquete;//este es el nombre del paquete, convirtiendo este archivo en el principal
 
 
-class Objeto {//creando una clase que identifica un tipo de variable, solo se puede hacer una clase publica por archivo y tiene que llamarse igual que el archivo
+class Objeto {//creando una clase que identifica un tipo de variable, solo se puede hacer una clase publica por archivo y tiene que llamarse igual que el archivo (se crea un .class por cada clase utilizada)
     private int edad;//una propiedad privada de la clase (solo se puede modificar desde dentro) (se suelen usar funciones set y get)
     String nombre;//igual pero se puede usar desde fuera
     public void Reportar(){//una funcin que hace la clase, actua sobre sus propiedades como objeto y puede acceder a propiedades privadas
@@ -11,10 +11,21 @@ class Objeto {//creando una clase que identifica un tipo de variable, solo se pu
     public static void Decir(){//una funcion que puede ser llamada sin hacer una variable de la clase (Objeto.Decir();)
         System.out.println("esto viene de una clase");
     }
+    public void setEdad(int edad){//metodos getter y setter para propiedades privadas
+        this.edad = edad;//usando this. se hace referencia al de la clase y no al parametro de la funcion
+    }
 
     public Objeto(){//metodo constructor para especificar el estado inicial de las propiedades
         edad = 0;
         nombre = "n";
+    }
+}
+class OtroObjeto{
+    int tamagno;
+    String nombre;
+    public OtroObjeto(int _tamagno, String _nombre){//poniendo parametros en el constructor se hace que al estanciar la clase se necesiten valores
+        tamagno = _tamagno;//tambien se podria usar el this.x
+        nombre = _nombre;
     }
 }
 
@@ -24,5 +35,7 @@ public class clases {//clase principal del paquete (el orden de declaracion de l
         Objeto obj = new Objeto();//declarar una variable para ese objeto
         obj.nombre = "aa";//acceder a las propiedades del objeto
         obj.Reportar();//ejecutar un metodo/funcion del objeto
+        OtroObjeto obj2 = new OtroObjeto(3, "asdf");//creando una instancia de un contstructor con parametros
+        
     }
 }
