f1ae008afae8d96ddd97469f8126d0e136ff41c2
yester <yesterdiscord7@gmail.com>
2023-12-19 21:23:40 +0100

    a

---
+++ Programa/src/varios/TemporizadorSimple.java
@@ -0,0 +1,14 @@
+package varios;
+import java.util.*;
+
+public class TemporizadorSimple {
+    static void temporizador(){
+        Timer temporizador = new Timer();
+        TimerTask tarea = new TimerTask() {
+            public void run(){//funcion a ejecutar
+                System.out.println("mensaje ocurrido en " + new Date());
+            }
+        };
+        temporizador.schedule(tarea, 0, 1000);//iniciar el temporizador (funcion run(), cuanto tarda en empezar, cada cuanto se ejecuta (0 para solo una vez))
+    }
+}
