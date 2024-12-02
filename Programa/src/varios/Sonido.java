0e2346c4c50ef7cf2c35aaf52146c8cafedf65dd
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 10:33:52 +0100

    Update Sonido.java

--- Programa/src/varios/Sonido.java
+++ Programa/src/varios/Sonido.java
@@ -7,7 +7,7 @@ import java.io.File;
 class Reproductor{
     public Clip sonido;
     public String nombre;
-    public void cargarSonido(String ruta){//mete el sonido en el reproductor (por ruta del proyecto) (una vez exportado, se tiene que poner el sonido tambien en la carpeta de la build ya que no se enpaquetan)
+    public void cargarSonido(String ruta){//Mete el sonido en el reproductor (por ruta del proyecto) (una vez exportado, se tiene que poner el sonido tambien en la carpeta de la build ya que no se enpaquetan)
         try{
             File archivo = new File(ruta);
             AudioInputStream audioStream = AudioSystem.getAudioInputStream(archivo);
@@ -17,19 +17,19 @@ class Reproductor{
             System.out.println("error");
         }
     }
-    public void reproducir(int frame){//reproduce el sonido a partir de x sample (0 = inicio)
+    public void reproducir(int frame){//Reproduce el sonido a partir de x sample (0 = inicio)
         if(sonido != null){
             sonido.setFramePosition(frame);
             sonido.start();
         }
     }
-    public void detener(){//detiene el sonido del todo
+    public void detener(){//Detiene el sonido del todo
         if(sonido != null && sonido.isRunning()){
             sonido.setFramePosition(0);
             sonido.stop();
         }
     }
-    public void cambiarFrame(int frame){//mueve el "cursor" por el audio
+    public void cambiarFrame(int frame){//Mueve el "cursor" por el audio
         if(sonido != null){
             sonido.setFramePosition(frame);
         }
